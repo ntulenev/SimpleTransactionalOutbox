c4efe8661df702429b181a0abd114506e03a0a89
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-29 20:46:30 +0300

    Add Data processing UOW

---
+++ src/SimpleTransactionalOutbox/Abstractions/DB/IProcessingDataUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Abstractions.Models;
+
+namespace Abstractions.DB
+{
+    interface IProcessingDataUnitOfWork : IUnitOfWork
+    {
+        Task ProcessDataAsync(IProcessingData data, CancellationToken cancellationToken = default);
+    }
+}
