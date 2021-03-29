55b65653f63fdb8cf9197a44119c9b6915171f96
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-29 20:47:14 +0300

    Add outbox sender

---
+++ src/SimpleTransactionalOutbox/Abstractions/Bus/IOutboxSender.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Abstractions.Models;
+
+namespace Abstractions.Bus
+{
+    public interface IOutboxSender
+    {
+        public Task SendAsync(IOutboxMessage message, CancellationToken cancellationToken = default);
+    }
+}
