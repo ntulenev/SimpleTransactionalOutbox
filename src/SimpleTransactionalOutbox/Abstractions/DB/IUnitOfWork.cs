08763e0221227f9a4c2d5ebd38a205c14ca0625f
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-29 20:45:57 +0300

    Add UOW

---
+++ src/SimpleTransactionalOutbox/Abstractions/DB/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Abstractions.DB
+{
+    public interface IUnitOfWork
+    {
+        Task SaveAsync(CancellationToken cancellationToken = default);
+    }
+}
