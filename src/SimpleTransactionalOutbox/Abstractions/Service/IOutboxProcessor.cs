e3556a712bcdeccef55decce8459ca1e1d178631
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-29 20:47:26 +0300

    Add outbox processor

---
+++ src/SimpleTransactionalOutbox/Abstractions/Service/IOutboxProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Abstractions.Models;
+
+namespace Abstractions.Service
+{
+    public interface IOutboxProcessor
+    {
+        Task ProcessAsync(IProcessingData data, CancellationToken cancellationToken = default);
+    }
+}
