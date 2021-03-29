753e5941f0771ccf8e0d393ee09e72279b0b0d54
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-29 20:46:41 +0300

    Add outbox UOW

---
+++ src/SimpleTransactionalOutbox/Abstractions/DB/IProcessingOutboxUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Abstractions.Models;
+
+namespace Abstractions.DB
+{
+    public interface IProcessingOutboxUnitOfWork : IUnitOfWork
+    {
+        Task<IReadOnlyCollection<IOutboxMessage>> ReadOutboxMessagesAsync(CancellationToken cancellationToken = default);
+
+        Task RemoveOutboxMessageAsync(IOutboxMessage message, CancellationToken cancellationToken = default);
+    }
+}
