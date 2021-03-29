4cba9f95a475332d88af43183dae35f701899eef
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-29 20:47:19 +0300

    Add data processor

---
+++ src/SimpleTransactionalOutbox/Abstractions/Service/IDataProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Abstractions.Models;
+
+namespace Abstractions.Service
+{
+    public interface IDataProcessor
+    {
+        Task ProcessDataAsync(IProcessingData data, CancellationToken cancellationToken = default);
+    }
+}
