a18a9ab9eb3f7e3341b2720c64abb17a9fc0af8c
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-30 20:34:47 +0300

    Add DbContext

---
+++ src/SimpleTransactionalOutbox/DB/OutboxContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DB
+{
+    public class OutboxContext : DbContext
+    {
+        public virtual DbSet<OutboxMessage> OutboxMessages { get; set; } = default!;
+
+        public virtual DbSet<ProcessingData> ProcessingData { get; set; } = default!;
+
+        public OutboxContext(DbContextOptions<OutboxContext> contextOptions)
+           : base(contextOptions)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSnakeCaseNamingConvention();
+
+        protected override void OnModelCreating([NotNull] ModelBuilder modelBuilder)
+        {
+            _ = modelBuilder.Entity<OutboxMessage>()
+                .HasKey(o => o.MessageId);
+
+            _ = modelBuilder.Entity<ProcessingData>()
+                .HasKey(e => e.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
