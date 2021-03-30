01392276025eb91a00c0b8399adbd76b5438f02d
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-30 20:50:14 +0300

    Add ISerializer<T>

---
+++ src/SimpleTransactionalOutbox/Infracructure/Serialization/ISerializer{T}.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infracructure.Serialization
+{
+    public interface ISerializer<T>
+    {
+        public string Serialize(T obj);
+    }
+}
