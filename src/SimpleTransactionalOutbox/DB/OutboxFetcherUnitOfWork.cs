172c268d2e95f5e851a70cfd9193fcedd2fcd65d
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-30 21:15:20 +0300

    Add OutboxFetcherUnitOfWork class

---
+++ src/SimpleTransactionalOutbox/DB/OutboxFetcherUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Abstractions.DB;
+using Abstractions.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class OutboxFetcherUnitOfWork : UnitOfWork<OutboxContext>, IOutboxFetcherUnitOfWork
+    {
+
+        public OutboxFetcherUnitOfWork(
+            OutboxContext context,
+            ILogger<OutboxFetcherUnitOfWork> logger)
+           : base(context, isolationLevel: null, logger)
+        {
+
+        }
+
+        public async Task<IReadOnlyCollection<IOutboxMessage>> ReadOutboxMessagesAsync(CancellationToken cancellationToken = default)
+        {
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+    }
+}
