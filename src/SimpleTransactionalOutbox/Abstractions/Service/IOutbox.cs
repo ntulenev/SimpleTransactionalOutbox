f382376864a3f85ea1c550da71da1c6b66e73b4c
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-31 19:11:16 +0300

    Add Outbox

---
+++ src/SimpleTransactionalOutbox/Abstractions/Service/IOutbox.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Abstractions.Service
+{
+    public interface IOutbox
+    {
+        public Task RunProcessingAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/SimpleTransactionalOutbox/Logic/Outbox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Abstractions.DB;
+using Abstractions.Service;
+
+
+namespace Logic
+{
+    public class Outbox : IOutbox
+    {
+        public Outbox(IOutboxFetcher fetcher,
+                      IServiceScopeFactory scopedFactory,
+                      ILogger<Outbox> logger
+                     )
+        {
+            _fetcher = fetcher ?? throw new ArgumentNullException(nameof(fetcher));
+            _scopedFactory = scopedFactory ?? throw new ArgumentNullException(nameof(scopedFactory));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        public async Task RunProcessingAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Run fetching messages from outbox.");
+            var messages = await _fetcher.ReadOutboxMessagesAsync(cancellationToken);
+
+            _logger.LogInformation("Messages count {count}", messages.Count);
+            foreach (var item in messages)
+            {
+                using var scope = _scopedFactory.CreateScope();
+                var processor = scope.ServiceProvider.GetRequiredService<IOutboxMessageProcessor>();
+
+                if (!await processor.TryProcessAsync(item, cancellationToken))
+                {
+                    _logger.LogWarning("Unable to process {@item}. Stopping processing and retry later.", item);
+                    break;
+                }
+            }
+
+            _logger.LogInformation("Finish processing messages.");
+        }
+
+        private readonly IOutboxFetcher _fetcher;
+        private readonly IServiceScopeFactory _scopedFactory;
+        private readonly ILogger<Outbox> _logger;
+
+    }
+}
