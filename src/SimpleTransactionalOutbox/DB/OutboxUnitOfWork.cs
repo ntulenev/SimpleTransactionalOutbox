292e3964474eeceecb41a65c89b2fc3dd0276642
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-31 18:48:27 +0300

    Add OutboxUnitOfWork

---
+++ src/SimpleTransactionalOutbox/DB/OutboxUnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Logging;
+
+using Abstractions.DB;
+using Abstractions.Models;
+
+namespace DB
+{
+    public class OutboxUnitOfWork : UnitOfWork<OutboxContext>, IOutboxUnitOfWork
+    {
+        public OutboxUnitOfWork(
+            OutboxContext context,
+            ILogger<ProcessingDataUnitOfWork> logger)
+           : base(context, System.Data.IsolationLevel.Serializable, logger)
+        {
+        }
+
+        public async Task RemoveOutboxMessageAsync(IOutboxMessage message, CancellationToken cancellationToken = default)
+        {
+
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            _logger.LogInformation("Removing message {@message}.", message);
+
+            var item = await _context.OutboxMessages.SingleAsync(x => x.MessageId == message.MessageId, cancellationToken).ConfigureAwait(false);
+
+            _context.OutboxMessages.Remove(item);
+        }
+    }
+}
