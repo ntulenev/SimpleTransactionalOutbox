9b805cc9efd8790e02895c8ea5d47c6fdfaa64ee
Nikita Tulenev <n1tulenev@gmail.com>
2021-03-31 08:00:23 +0300

    Add outbox fetcher query

--- src/SimpleTransactionalOutbox/DB/OutboxFetcher.cs
+++ src/SimpleTransactionalOutbox/DB/OutboxFetcher.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 using Abstractions.DB;
 using Abstractions.Models;
+using System.Linq;
 
 namespace DB
 {
@@ -14,21 +15,24 @@ namespace DB
     {
 
         public OutboxFetcher(
-            OutboxContext context,
-            ILogger<OutboxFetcher> logger)
+            OutboxContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task<IReadOnlyCollection<IOutboxMessage>> ReadOutboxMessagesAsync(CancellationToken cancellationToken = default)
         {
-            await Task.Yield();
-            throw new NotImplementedException();
+            return await _context.OutboxMessages
+                                 .OrderBy(x => x.OccurredOn)
+                                 .Take(LIMIT)
+                                 .Select(x => new Models.OutboxMessage(x.MessageId, x.OccurredOn, x.MessageType, x.Body))
+                                 .ToListAsync(cancellationToken)
+                                 .ConfigureAwait(false);
         }
 
         private readonly OutboxContext _context;
-        private readonly ILogger<OutboxFetcher> _logger;
+
+        private const int LIMIT = 10;
 
     }
 }
