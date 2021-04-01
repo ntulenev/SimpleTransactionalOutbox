dd31b59e212db6e76fc260e333d5943b61aa96b9
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-01 18:33:24 +0300

    Add ProcessingData

---
+++ src/Models/ProcessingData.cs
@@ -0,0 +1,17 @@
+﻿using Abstractions.Models;
+
+namespace Models
+{
+    public class ProcessingData : IProcessingData
+    {
+        public long Id { get; }
+
+        public int Value { get; }
+
+        public ProcessingData(int id, int value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
