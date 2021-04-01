0a6ecc70545b274cdbbedd891e62a35999fec8ea
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-01 18:31:33 +0300

    Add Json serializer

---
+++ src/Serialization/JsonSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+using Abstractions.Serialization;
+
+namespace Serialization
+{
+    public class JsonSerializer<T> : ISerializer<T>
+    {
+        public string Serialize(T obj)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
