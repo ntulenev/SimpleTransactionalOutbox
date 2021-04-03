fa51cb422985e5039b6c2bb88b8089e51c472b34
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:01:11 +0300

    Add IOutboxMessageProcessor comments

--- src/Abstractions/Service/IOutboxMessageProcessor.cs
+++ src/Abstractions/Service/IOutboxMessageProcessor.cs
@@ -5,8 +5,17 @@ using Abstractions.Models;
 
 namespace Abstractions.Service
 {
+    /// <summary>
+    /// Processor for single outbox message.
+    /// </summary>
     public interface IOutboxMessageProcessor
     {
+        /// <summary>
+        /// Attempts to process single outbox message.
+        /// </summary>
+        /// <param name="message">Outbox message for processing.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Status of operation (true if success, false othervise).</returns>
         Task<bool> TryProcessAsync(IOutboxMessage message, CancellationToken cancellationToken = default);
     }
 }
