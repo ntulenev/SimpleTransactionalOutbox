1e882bb4a3c0345f672d363d5d348976f429615e
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:09:47 +0300

    Add ProcessingData comments

--- src/DB/ProcessingData.cs
+++ src/DB/ProcessingData.cs
@@ -1,9 +1,18 @@
 ﻿namespace DB
 {
+    /// <summary>
+    /// DB Processing data model.
+    /// </summary>
     public class ProcessingData
     {
+        /// <summary>
+        /// Processing data id.
+        /// </summary>
         public long Id { get; set; }
 
+        /// <summary>
+        /// Processing data value.
+        /// </summary>
         public int Value { get; set; }
     }
 }
