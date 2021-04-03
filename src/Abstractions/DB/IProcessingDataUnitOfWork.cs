2e565d7ec963c9afd096e4caaad978ece950de3a
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:00:02 +0300

    Add IProcessingDataUnitOfWork comments

--- src/Abstractions/DB/IProcessingDataUnitOfWork.cs
+++ src/Abstractions/DB/IProcessingDataUnitOfWork.cs
@@ -5,8 +5,16 @@ using Abstractions.Models;
 
 namespace Abstractions.DB
 {
+    /// <summary>
+    /// <see cref="IUnitOfWork"/> for processing logic.
+    /// </summary>
     public interface IProcessingDataUnitOfWork : IUnitOfWork
     {
+        /// <summary>
+        /// Addes new <paramref name="data"/> to the stotage.
+        /// </summary>
+        /// <param name="data">Processing data.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         Task ProcessDataAsync(IProcessingData data, CancellationToken cancellationToken = default);
     }
 }
