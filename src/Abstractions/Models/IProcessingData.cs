3393467893b62d9daf757c710f830b1187e6ca43
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:00:26 +0300

    Add IProcessingData comments

--- src/Abstractions/Models/IProcessingData.cs
+++ src/Abstractions/Models/IProcessingData.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Abstractions.Models
+﻿namespace Abstractions.Models
 {
+    /// <summary>
+    /// Data model interface.
+    /// </summary>
     public interface IProcessingData
     {
+        /// <summary>
+        /// Id of the processing data.
+        /// </summary>
         public long Id { get; }
 
+        /// <summary>
+        /// Value of the proccesing data.
+        /// </summary>
         public int Value { get; }
     }
 }
