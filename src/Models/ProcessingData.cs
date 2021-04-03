9f7f2c52ad290f0795da0af5ba7e9355ca932292
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:16:58 +0300

    Add ProcessingData comments

--- src/Models/ProcessingData.cs
+++ src/Models/ProcessingData.cs
@@ -2,12 +2,22 @@
 
 namespace Models
 {
+    /// <summary>
+    /// Processing data message.
+    /// </summary>
     public class ProcessingData : IProcessingData
     {
+        /// <inheritdoc/>
         public long Id { get; }
 
+        /// <inheritdoc/>
         public int Value { get; }
 
+        /// <summary>
+        /// Creates <see cref="ProcessingData"/>.
+        /// </summary>
+        /// <param name="id">Data id.</param>
+        /// <param name="value">Data value.</param>
         public ProcessingData(int id, int value)
         {
             Id = id;
