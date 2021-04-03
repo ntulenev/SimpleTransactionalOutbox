54ecc7e448765ba9c828cf6f006690c10cc0da60
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 06:59:34 +0300

    Add IOutboxFetcher comments

--- src/Abstractions/Bus/IOutboxSender.cs
+++ src/Abstractions/Bus/IOutboxSender.cs
@@ -5,8 +5,16 @@ using Abstractions.Models;
 
 namespace Abstractions.Bus
 {
+    /// <summary>
+    /// Message publisher.
+    /// </summary>
     public interface IOutboxSender
     {
+        /// <summary>
+        /// Publishes <paramref name="message"/> to external system.
+        /// </summary>
+        /// <param name="message">Message to publish.</param>
+        /// <param name="cancellationToken">Canellation token.</param>
         public Task SendAsync(IOutboxMessage message, CancellationToken cancellationToken = default);
     }
 }
