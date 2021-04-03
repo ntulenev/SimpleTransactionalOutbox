bcd2d5643794dbb3dec9db580531b6a9a933f1b0
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:00:49 +0300

    Add ISerializer comments

--- src/Abstractions/Serialization/ISerializer{T}.cs
+++ src/Abstractions/Serialization/ISerializer{T}.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Abstractions.Serialization
+﻿namespace Abstractions.Serialization
 {
+    /// <summary>
+    /// Serialize model to string representation.
+    /// </summary>
+    /// <typeparam name="T">Model type.</typeparam>
     public interface ISerializer<T>
     {
+        /// <summary>
+        /// Serializes model to string format.
+        /// </summary>
+        /// <param name="obj">Model to be serialized.</param>
+        /// <returns>String representation of the model.</returns>
         public string Serialize(T obj);
     }
 }
