7e7e723a83b4fa988c244b426f4d1b0363bea3f2
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:00:16 +0300

    Add IOutboxMessage comments

--- src/Abstractions/Models/IOutboxMessage.cs
+++ src/Abstractions/Models/IOutboxMessage.cs
@@ -2,14 +2,29 @@
 
 namespace Abstractions.Models
 {
+    /// <summary>
+    /// Outbox message interface.
+    /// </summary>
     public interface IOutboxMessage
     {
+        /// <summary>
+        /// Id of the message.
+        /// </summary>
         public Guid MessageId { get; }
 
+        /// <summary>
+        /// Creation date/time.
+        /// </summary>
         public DateTime OccurredOn { get; }
 
+        /// <summary>
+        /// Type of outbox message.
+        /// </summary>
         public OutboxMessageType MessageType { get; }
 
+        /// <summary>
+        /// Message body.
+        /// </summary>
         public string Body { get; }  
     }
 
