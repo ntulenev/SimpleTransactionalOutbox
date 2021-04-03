9c30bacb16852809626a292aef7d221c7bc0eea9
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 06:59:53 +0300

    Add IOutboxUnitOfWork comments

--- src/Abstractions/DB/IOutboxUnitOfWork.cs
+++ src/Abstractions/DB/IOutboxUnitOfWork.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 using Abstractions.Models;
 
 namespace Abstractions.DB
 {
+    /// <summary>
+    /// <see cref="IUnitOfWork"/> for outbox logic.
+    /// </summary>
     public interface IOutboxUnitOfWork : IUnitOfWork
     {
+        /// <summary>
+        /// Deletes not actual outbox message.
+        /// </summary>
+        /// <param name="message">Outbox message.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         Task RemoveOutboxMessageAsync(IOutboxMessage message, CancellationToken cancellationToken = default);
     }
 }
