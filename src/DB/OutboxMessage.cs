0954b535228d243085bde0254317e5e30ad31f3a
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:09:32 +0300

    Add OutboxMessage comments

--- src/DB/OutboxMessage.cs
+++ src/DB/OutboxMessage.cs
@@ -4,16 +4,34 @@ using Abstractions.Models;
 
 namespace DB
 {
+    /// <summary>
+    /// DB Outbox message model.
+    /// </summary>
     public class OutboxMessage
     {
+        /// <summary>
+        /// Message Id.
+        /// </summary>
         public Guid MessageId { get; }
 
+        /// <summary>
+        /// Message created date/time.
+        /// </summary>
         public DateTime OccurredOn { get; set; }
 
+        /// <summary>
+        /// Message type.
+        /// </summary>
         public OutboxMessageType MessageType { get; set; }
 
+        /// <summary>
+        /// Message body.
+        /// </summary>
         public string Body { get; set; } = default!;
 
+        /// <summary>
+        /// Creates <see cref="OutboxMessage"/>.
+        /// </summary>
         public OutboxMessage()
         {
             MessageId = Guid.NewGuid();
