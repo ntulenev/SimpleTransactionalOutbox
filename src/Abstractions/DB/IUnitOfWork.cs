b374f3c1fe829b0a7d4dd1248ced9d3f7be250ab
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:00:09 +0300

    Add IUnitOfWork comments

--- src/Abstractions/DB/IUnitOfWork.cs
+++ src/Abstractions/DB/IUnitOfWork.cs
@@ -3,8 +3,15 @@ using System.Threading.Tasks;
 
 namespace Abstractions.DB
 {
+    /// <summary>
+    /// Interface that represents UnitOfWork pattern.
+    /// </summary>
     public interface IUnitOfWork
     {
+        /// <summary>
+        /// Commits operation results to the storage.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
         Task SaveAsync(CancellationToken cancellationToken = default);
     }
 }
