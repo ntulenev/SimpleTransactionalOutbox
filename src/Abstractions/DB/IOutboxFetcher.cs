649af0a0df756acf215089fbf84d5a0edbc3c511
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 06:59:44 +0300

    Add IOutboxFetcher comments

--- src/Abstractions/DB/IOutboxFetcher.cs
+++ src/Abstractions/DB/IOutboxFetcher.cs
@@ -6,8 +6,16 @@ using Abstractions.Models;
 
 namespace Abstractions.DB
 {
+    /// <summary>
+    /// Outbox messages fetcher.
+    /// </summary>
     public interface IOutboxFetcher
     {
+        /// <summary>
+        /// Pools actual outbox messages from storage.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Collection of actual outbox messages</returns>
         Task<IReadOnlyCollection<IOutboxMessage>> ReadOutboxMessagesAsync(CancellationToken cancellationToken = default);
     }
 }
