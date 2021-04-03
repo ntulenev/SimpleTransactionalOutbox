0a693883b44576f63a891f65fcfdb124eda78bf3
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:00:57 +0300

    Add IDataProcessor comments

--- src/Abstractions/Service/IDataProcessor.cs
+++ src/Abstractions/Service/IDataProcessor.cs
@@ -5,8 +5,16 @@ using Abstractions.Models;
 
 namespace Abstractions.Service
 {
+    /// <summary>
+    /// Processor for incommind data message.
+    /// </summary>
     public interface IDataProcessor
     {
+        /// <summary>
+        /// Handles new data item.
+        /// </summary>
+        /// <param name="data">Processing data item to be added to the system.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         Task ProcessDataAsync(IProcessingData data, CancellationToken cancellationToken = default);
     }
 }
