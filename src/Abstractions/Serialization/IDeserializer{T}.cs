75332faa0c339cbe4993ca2f3598cdb0e3d1b169
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:00:42 +0300

    Add IDeserializer comments

--- src/Abstractions/Serialization/IDeserializer{T}.cs
+++ src/Abstractions/Serialization/IDeserializer{T}.cs
@@ -1,7 +1,16 @@
 ﻿namespace Abstractions.Serialization
 {
+    /// <summary>
+    /// Deserializer string to model representation.
+    /// </summary>
+    /// <typeparam name="T">Model type.</typeparam>
     public interface IDeserializer<T>
     {
+        /// <summary>
+        /// Deserializes string to <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="obj">Data in string format.</param>
+        /// <returns>Deserialized model.</returns>
         public T Deserialize(string obj);
     }
 }
