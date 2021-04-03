14051c828a15acf76b3a85a4fab2a7215f509928
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-03 07:01:05 +0300

    Add IOutbox comments

--- src/Abstractions/Service/IOutbox.cs
+++ src/Abstractions/Service/IOutbox.cs
@@ -3,8 +3,15 @@ using System.Threading.Tasks;
 
 namespace Abstractions.Service
 {
+    /// <summary>
+    /// Outbox service interface.
+    /// </summary>
     public interface IOutbox
     {
+        /// <summary>
+        /// Processes outbox work phase.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public Task RunProcessingAsync(CancellationToken cancellationToken);
     }
 }
