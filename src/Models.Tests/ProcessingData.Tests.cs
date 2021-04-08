e9e835e880c3457b0303793350f81fc05fc4a880
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-08 19:48:43 +0300

    Add ProcessingData Tests

---
+++ src/Models.Tests/ProcessingData.Tests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace Models.Tests
+{
+    public class ProcessingDataTests
+    {
+        [Fact(DisplayName = " ProcessingData can be created.")]
+        [Trait("Category", "Unit")]
+        public void OutboxMessageShouldBeCreatedWithValidParams()
+        {
+            // Arrange
+            int id = 42;
+            int value = 5;
+
+            // Act
+            ProcessingData res = null!;
+            var exception = Record.Exception(() => res = new ProcessingData(id, value));
+
+            // Assert
+            exception.Should().BeNull();
+
+            res.Id.Should().Be(id);
+            res.Value.Should().Be(value);
+
+        }
+    }
+}
