4805d825f16540e89198ce37dc347d614845cbf6
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-08 20:00:36 +0300

    Add JsonDeserializer Tests

---
+++ src/Serialization.Tests/JsonDeserializer{T}.Tests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Serialization.Tests
+{
+    public class JsonDeserializerTests
+    {
+        [Fact(DisplayName = "JsonDeserializer can Deserialize data.")]
+        [Trait("Category", "Unit")]
+        public void CanDeserialize()
+        {
+
+            // Arrange
+            var deserializer = new JsonDeserializer<TestClass>();
+            var testString = "{ \"test_value\" : \"test 123\", \"test_id\" : \"42\" }";
+
+            // Act
+            TestClass result = null!;
+            var exception = Record.Exception(() => result = deserializer.Deserialize(testString));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Id.Should().Be(42);
+            result.Value.Should().Be("test 123");
+        }
+
+        [Fact(DisplayName = "JsonDeserializer cant Deserialize null.")]
+        [Trait("Category", "Unit")]
+        public void CantDeserializeNull()
+        {
+
+            // Arrange
+            var deserializer = new JsonDeserializer<TestClass>();
+            string testString = null!;
+
+            // Act
+            TestClass result = null!;
+            var exception = Record.Exception(() => result = deserializer.Deserialize(testString));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+    }
+}
---
+++ src/Serialization.Tests/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Serialization.Tests
+{
+    class TestClass
+    {
+        [JsonProperty("test_id")]
+        public int Id { get; set; }
+
+        [JsonProperty("test_value")]
+        public string Value { get; set; } = default!;
+    }
+}
