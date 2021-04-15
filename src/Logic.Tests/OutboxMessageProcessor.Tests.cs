d54472796557528de051c18bc40cf6e0283c3c9b
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-15 18:54:32 +0300

    Add TryProcessAsync tests

--- src/Logic.Tests/OutboxMessageProcessor.Tests.cs
+++ src/Logic.Tests/OutboxMessageProcessor.Tests.cs
@@ -105,5 +105,113 @@ namespace Logic.Tests
             // Assert
             exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
         }
+
+        [Fact(DisplayName = "OutboxMessageProcessor returns false on send error.")]
+        [Trait("Category", "Unit")]
+        public async Task ProcessWithFalseOnSendError()
+        {
+
+            // Arrange
+            var uow = new Mock<IOutboxUnitOfWork>();
+            var sender = new Mock<IOutboxSender>();
+            var ilogger = new Mock<ILogger<OutboxMessageProcessor>>();
+            var processor = new OutboxMessageProcessor(uow.Object, sender.Object, ilogger.Object);
+            var cts = new CancellationTokenSource();
+            var message = new Mock<IOutboxMessage>();
+
+            sender.Setup(x => x.SendAsync(message.Object, cts.Token)).ThrowsAsync(new Exception());
+
+
+            bool result = true;
+            // Act
+            var exception = await Record.ExceptionAsync(async () => result = await processor.TryProcessAsync(message.Object, cts.Token));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeFalse();
+        }
+
+        [Fact(DisplayName = "OutboxMessageProcessor returns false on remove error.")]
+        [Trait("Category", "Unit")]
+        public async Task ProcessWithFalseOnRemoveError()
+        {
+
+            // Arrange
+            var uow = new Mock<IOutboxUnitOfWork>();
+            var sender = new Mock<IOutboxSender>();
+            var ilogger = new Mock<ILogger<OutboxMessageProcessor>>();
+            var processor = new OutboxMessageProcessor(uow.Object, sender.Object, ilogger.Object);
+            var cts = new CancellationTokenSource();
+            var message = new Mock<IOutboxMessage>();
+
+            uow.Setup(x => x.RemoveOutboxMessageAsync(message.Object, cts.Token)).ThrowsAsync(new Exception());
+
+
+            bool result = true;
+            // Act
+            var exception = await Record.ExceptionAsync(async () => result = await processor.TryProcessAsync(message.Object, cts.Token));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeFalse();
+        }
+
+
+        [Fact(DisplayName = "OutboxMessageProcessor returns false on save error.")]
+        [Trait("Category", "Unit")]
+        public async Task ProcessWithFalseOnSaveError()
+        {
+
+            // Arrange
+            var uow = new Mock<IOutboxUnitOfWork>();
+            var sender = new Mock<IOutboxSender>();
+            var ilogger = new Mock<ILogger<OutboxMessageProcessor>>();
+            var processor = new OutboxMessageProcessor(uow.Object, sender.Object, ilogger.Object);
+            var cts = new CancellationTokenSource();
+            var message = new Mock<IOutboxMessage>();
+
+            uow.Setup(x => x.SaveAsync(cts.Token)).ThrowsAsync(new Exception());
+
+
+            bool result = true;
+            // Act
+            var exception = await Record.ExceptionAsync(async () => result = await processor.TryProcessAsync(message.Object, cts.Token));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeFalse();
+        }
+
+        [Fact(DisplayName = "OutboxMessageProcessor returns true on correct process.")]
+        [Trait("Category", "Unit")]
+        public async Task ProcessWithTrueOnAllSucceed()
+        {
+
+            // Arrange
+            var uow = new Mock<IOutboxUnitOfWork>();
+            var sender = new Mock<IOutboxSender>();
+            var ilogger = new Mock<ILogger<OutboxMessageProcessor>>();
+            var processor = new OutboxMessageProcessor(uow.Object, sender.Object, ilogger.Object);
+            var cts = new CancellationTokenSource();
+            var message = new Mock<IOutboxMessage>();
+
+            int callOrder = 0;
+            sender.Setup(x => x.SendAsync(message.Object, cts.Token)).Callback(() => callOrder++.Should().Be(0));
+            uow.Setup(x => x.RemoveOutboxMessageAsync(message.Object, cts.Token)).Callback(() => callOrder++.Should().Be(1));
+            uow.Setup(x => x.SaveAsync(cts.Token)).Callback(() => callOrder++.Should().Be(2));
+
+
+            bool result = true;
+            // Act
+            var exception = await Record.ExceptionAsync(async () => result = await processor.TryProcessAsync(message.Object, cts.Token));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().BeTrue();
+
+            sender.Verify(x => x.SendAsync(message.Object, cts.Token), Times.Once);
+            uow.Verify(x => x.RemoveOutboxMessageAsync(message.Object, cts.Token), Times.Once);
+            uow.Verify(x => x.SaveAsync(cts.Token), Times.Once);
+        }
     }
 }
