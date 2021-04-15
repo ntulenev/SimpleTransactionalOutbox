6eb19ffa93955b4dd0892fd89e870142bddf1c3b
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-15 18:59:23 +0300

    Add OutboxTests

---
+++ src/Logic.Tests/Outbox.Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Xunit;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using Abstractions.DB;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logic.Tests
+{
+	public class OutboxTests
+	{
+
+		[Fact(DisplayName = "Outbox cant be created with null fetcher.")]
+		[Trait("Category", "Unit")]
+		public void CantCreateWithNullFetcher()
+		{
+			// Arrange
+			var fetcher = (IOutboxFetcher)null!;
+			var scopedFactory = new Mock<IServiceScopeFactory>();
+			var ilogger = new Mock<ILogger<Outbox>>();
+
+			// Act
+			var exception = Record.Exception(() => new Outbox(fetcher, scopedFactory.Object, ilogger.Object));
+
+			// Assert
+			exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+		}
+	}
+}
