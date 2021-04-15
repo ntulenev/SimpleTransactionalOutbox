a368ba5c497c68e7dc8ff16bb6e5a8e70ca45d11
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-15 19:05:21 +0300

    Add Outbox Tests

--- src/Logic.Tests/Outbox.Tests.cs
+++ src/Logic.Tests/Outbox.Tests.cs
@@ -32,5 +32,38 @@ namespace Logic.Tests
 			// Assert
 			exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
 		}
+
+
+		[Fact(DisplayName = "Outbox cant be created with null scopedFactory.")]
+		[Trait("Category", "Unit")]
+		public void CantCreateWithNullFactory()
+		{
+			// Arrange
+			var fetcher =  new Mock<IOutboxFetcher>();
+			var scopedFactory = (IServiceScopeFactory)null!;
+			var ilogger = new Mock<ILogger<Outbox>>();
+
+			// Act
+			var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory, ilogger.Object));
+
+			// Assert
+			exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+		}
+
+		[Fact(DisplayName = "Outbox cant be created with null Logger.")]
+		[Trait("Category", "Unit")]
+		public void CantCreateWithNullLogger()
+		{
+			// Arrange
+			var fetcher = new Mock<IOutboxFetcher>();
+			var scopedFactory = new Mock<IServiceScopeFactory>();
+			var ilogger = (ILogger<Outbox>)null!;
+
+			// Act
+			var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory.Object, ilogger));
+
+			// Assert
+			exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+		}
 	}
 }
