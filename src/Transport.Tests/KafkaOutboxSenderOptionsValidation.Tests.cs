6a97e3ccaa9e788250b6a7c875904aa3abc711e5
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-16 22:06:20 +0300

    Add KafkaOutboxSenderOptionsValidation.Tests

---
+++ src/Transport.Tests/KafkaOutboxSenderOptionsValidation.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+using FluentAssertions;
+
+using Transport.Validation;
+
+namespace Transport.Tests
+{
+    public class KafkaOutboxSenderOptionsValidationTests
+    {
+        [Fact(DisplayName = "KafkaOutboxSenderOptionsValidation can be created.")]
+        [Trait("Category", "Unit")]
+        public void CanBeCreated()
+        {
+
+            // Act
+            var exception = Record.Exception(() => new KafkaOutboxSenderOptionsValidation());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}
