717e2f95b0be685886b94b73ed91d341c21052eb
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-16 22:40:53 +0300

    Add KafkaOutboxSender Tests

--- src/Transport.Tests/KafkaOutboxSender.Tests.cs
+++ src/Transport.Tests/KafkaOutboxSender.Tests.cs
@@ -29,10 +29,101 @@ namespace Transport.Tests
             var logger = new Mock<ILogger<KafkaOutboxSender>>();
 
             // Act
-            var exception = Record.Exception(() => new KafkaOutboxSender(producer, serializer.Object,options.Object,logger.Object));
+            var exception = Record.Exception(() => new KafkaOutboxSender(producer, serializer.Object, options.Object, logger.Object));
 
             // Assert
             exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "KafkaOutboxSender cant be created with null serializer.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullSerializer()
+        {
+
+            // Arrange
+            var producer = new Mock<IProducer<Null, string>>();
+            var serializer = (Abstractions.Serialization.ISerializer<IOutboxMessage>)null!;
+            var options = new Mock<IOptions<KafkaOutboxSenderOptions>>();
+            var logger = new Mock<ILogger<KafkaOutboxSender>>();
+
+            // Act
+            var exception = Record.Exception(() => new KafkaOutboxSender(producer.Object, serializer, options.Object, logger.Object));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "KafkaOutboxSender cant be created with null options.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullOptions()
+        {
+
+            // Arrange
+            var producer = new Mock<IProducer<Null, string>>();
+            var serializer = new Mock<Abstractions.Serialization.ISerializer<IOutboxMessage>>();
+            var options = (IOptions<KafkaOutboxSenderOptions>)null!;
+            var logger = new Mock<ILogger<KafkaOutboxSender>>();
+
+            // Act
+            var exception = Record.Exception(() => new KafkaOutboxSender(producer.Object, serializer.Object, options, logger.Object));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "KafkaOutboxSender cant be created with null logger.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullLogger()
+        {
+
+            // Arrange
+            var producer = new Mock<IProducer<Null, string>>();
+            var serializer = new Mock<Abstractions.Serialization.ISerializer<IOutboxMessage>>();
+            var options = new Mock<IOptions<KafkaOutboxSenderOptions>>();
+            var logger = (ILogger<KafkaOutboxSender>)null!;
+
+            // Act
+            var exception = Record.Exception(() => new KafkaOutboxSender(producer.Object, serializer.Object, options.Object, logger));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "KafkaOutboxSender cant be created with null options value.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullOptionsValue()
+        {
+
+            // Arrange
+            var producer = new Mock<IProducer<Null, string>>();
+            var serializer = new Mock<Abstractions.Serialization.ISerializer<IOutboxMessage>>();
+            var options = new Mock<IOptions<KafkaOutboxSenderOptions>>();
+            var logger = new Mock<ILogger<KafkaOutboxSender>>();
+
+            // Act
+            var exception = Record.Exception(() => new KafkaOutboxSender(producer.Object, serializer.Object, options.Object, logger.Object));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "KafkaOutboxSender can be created with valid params.")]
+        [Trait("Category", "Unit")]
+        public void CanCreate()
+        {
+
+            // Arrange
+            var producer = new Mock<IProducer<Null, string>>();
+            var serializer = new Mock<Abstractions.Serialization.ISerializer<IOutboxMessage>>();
+            var options = new Mock<IOptions<KafkaOutboxSenderOptions>>();
+            options.Setup(x => x.Value).Returns(new KafkaOutboxSenderOptions() { TopicName = "A" });
+            var logger = new Mock<ILogger<KafkaOutboxSender>>();
+
+            // Act
+            var exception = Record.Exception(() => new KafkaOutboxSender(producer.Object, serializer.Object, options.Object, logger.Object));
+
+            // Assert
+            exception.Should().BeNull();
         }
 
     }
