fd02a58c1b71e3d0799e322deeaa0b01f866c5c5
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-16 20:33:58 +0300

    Add RunProcessingAsync Test

--- src/Logic.Tests/Outbox.Tests.cs
+++ src/Logic.Tests/Outbox.Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 using Xunit;
 
@@ -10,76 +11,110 @@ using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
 using Abstractions.DB;
-
+using System.Threading;
+using Abstractions.Service;
 
 namespace Logic.Tests
 {
-	public class OutboxTests
-	{
+    public class OutboxTests
+    {
 
-		[Fact(DisplayName = "Outbox cant be created with null fetcher.")]
-		[Trait("Category", "Unit")]
-		public void CantCreateWithNullFetcher()
-		{
-			// Arrange
-			var fetcher = (IOutboxFetcher)null!;
-			var scopedFactory = new Mock<IServiceScopeFactory>();
-			var ilogger = new Mock<ILogger<Outbox>>();
+        [Fact(DisplayName = "Outbox cant be created with null fetcher.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullFetcher()
+        {
+            // Arrange
+            var fetcher = (IOutboxFetcher)null!;
+            var scopedFactory = new Mock<IServiceScopeFactory>();
+            var ilogger = new Mock<ILogger<Outbox>>();
 
-			// Act
-			var exception = Record.Exception(() => new Outbox(fetcher, scopedFactory.Object, ilogger.Object));
+            // Act
+            var exception = Record.Exception(() => new Outbox(fetcher, scopedFactory.Object, ilogger.Object));
 
-			// Assert
-			exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
-		}
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
 
 
-		[Fact(DisplayName = "Outbox cant be created with null scopedFactory.")]
-		[Trait("Category", "Unit")]
-		public void CantCreateWithNullFactory()
-		{
-			// Arrange
-			var fetcher =  new Mock<IOutboxFetcher>();
-			var scopedFactory = (IServiceScopeFactory)null!;
-			var ilogger = new Mock<ILogger<Outbox>>();
+        [Fact(DisplayName = "Outbox cant be created with null scopedFactory.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullFactory()
+        {
+            // Arrange
+            var fetcher = new Mock<IOutboxFetcher>();
+            var scopedFactory = (IServiceScopeFactory)null!;
+            var ilogger = new Mock<ILogger<Outbox>>();
 
-			// Act
-			var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory, ilogger.Object));
+            // Act
+            var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory, ilogger.Object));
 
-			// Assert
-			exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
-		}
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
 
-		[Fact(DisplayName = "Outbox cant be created with null Logger.")]
-		[Trait("Category", "Unit")]
-		public void CantCreateWithNullLogger()
-		{
-			// Arrange
-			var fetcher = new Mock<IOutboxFetcher>();
-			var scopedFactory = new Mock<IServiceScopeFactory>();
-			var ilogger = (ILogger<Outbox>)null!;
+        [Fact(DisplayName = "Outbox cant be created with null Logger.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullLogger()
+        {
+            // Arrange
+            var fetcher = new Mock<IOutboxFetcher>();
+            var scopedFactory = new Mock<IServiceScopeFactory>();
+            var ilogger = (ILogger<Outbox>)null!;
 
-			// Act
-			var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory.Object, ilogger));
+            // Act
+            var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory.Object, ilogger));
 
-			// Assert
-			exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
-		}
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
 
-		[Fact(DisplayName = "Outbox can be created with valid params.")]
-		[Trait("Category", "Unit")]
-		public void CanCreate()
-		{
-			// Arrange
-			var fetcher = new Mock<IOutboxFetcher>();
-			var scopedFactory = new Mock<IServiceScopeFactory>();
-			var ilogger = new Mock<ILogger<Outbox>>();
+        [Fact(DisplayName = "Outbox can be created with valid params.")]
+        [Trait("Category", "Unit")]
+        public void CanCreate()
+        {
+            // Arrange
+            var fetcher = new Mock<IOutboxFetcher>();
+            var scopedFactory = new Mock<IServiceScopeFactory>();
+            var ilogger = new Mock<ILogger<Outbox>>();
 
-			// Act
-			var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory.Object, ilogger.Object));
+            // Act
+            var exception = Record.Exception(() => new Outbox(fetcher.Object, scopedFactory.Object, ilogger.Object));
 
-			// Assert
-			exception.Should().BeNull();
-		}
-	}
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "Outbox process messages correctly.")]
+        [Trait("Category", "Unit")]
+        public async Task RunProcessingAsyncProcessMessagesCorrectlyAsync()
+        {
+            // Arrange
+            var fetcher = new Mock<IOutboxFetcher>();
+            var scopedFactory = new Mock<IServiceScopeFactory>();
+            var ilogger = new Mock<ILogger<Outbox>>();
+            var outBox = new Outbox(fetcher.Object, scopedFactory.Object, ilogger.Object);
+            var tokenSource = new CancellationTokenSource();
+
+            var msg = new TestMessage();
+            fetcher.Setup(x => x.ReadOutboxMessagesAsync(tokenSource.Token)).ReturnsAsync(new[] { msg });
+            var scope = new Mock<IServiceScope>();
+            scopedFactory.Setup(x => x.CreateScope()).Returns(scope.Object);
+            var provider = new Mock<IServiceProvider>();
+            scope.Setup(x => x.ServiceProvider).Returns(provider.Object);
+            var processor = new Mock<IOutboxMessageProcessor>();
+            provider.Setup(x => x.GetService(typeof(IOutboxMessageProcessor))).Returns(processor.Object);
+
+            // Act
+            var exception = await Record.ExceptionAsync(async () => await outBox.RunProcessingAsync(tokenSource.Token));
+
+            // Assert
+            exception.Should().BeNull();
+            fetcher.Verify(x => x.ReadOutboxMessagesAsync(tokenSource.Token), Times.Once);
+            scopedFactory.Verify(x => x.CreateScope(), Times.Once);
+            scope.Verify(x => x.ServiceProvider, Times.Once);
+            provider.Verify(x => x.GetService(typeof(IOutboxMessageProcessor)), Times.Once);
+            processor.Verify(x => x.TryProcessAsync(msg, tokenSource.Token), Times.Once);
+            scope.Verify(x => x.Dispose(), Times.Once);
+        }
+    }
 }
---
+++ src/Logic.Tests/TestMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Abstractions.Models;
+
+namespace Logic.Tests
+{
+    public class TestMessage : IOutboxMessage
+    {
+        public Guid MessageId { get; set; }
+
+        public DateTime OccurredOn { get; set; }
+
+        public OutboxMessageType MessageType { get; set; }
+
+        public string Body { get; set; } = default!;
+    }
+}
