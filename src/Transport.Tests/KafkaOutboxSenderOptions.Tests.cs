e1343df713ebfee7f96d5974ab2749c1f19e0737
Nikita Tulenev <n1tulenev@gmail.com>
2021-04-16 21:11:51 +0300

    Add KafkaOutboxSenderOptions Tests

---
+++ src/Transport.Tests/KafkaOutboxSenderOptions.Tests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace Transport.Tests
+{
+    public class KafkaOutboxSenderOptionsTests
+    {
+        [Fact(DisplayName = "KafkaOutboxSenderOptions can be created.")]
+        [Trait("Category", "Unit")]
+        public void CanBeCreated()
+        {
+
+            // Act
+            var exception = Record.Exception(() => new KafkaOutboxSenderOptions());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "KafkaOutboxSenderOptions can get an set topic name.")]
+        [Trait("Category", "Unit")]
+        public void CanGetAndSetTopicName()
+        {
+            // Arrange
+            var topicName = "data";
+            var options = new KafkaOutboxSenderOptions();
+
+            // Act
+            var exception = Record.Exception(() => options.TopicName = topicName);
+
+            // Assert
+            exception.Should().BeNull();
+            options.TopicName.Should().Be(topicName);
+        }
+    }
+}
