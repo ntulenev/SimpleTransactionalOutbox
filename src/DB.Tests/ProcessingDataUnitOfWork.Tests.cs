79ebabc8b1314e62af03b9fbc786eaa04921cbea
Nikita Tulenev <n1tulenev@gmail.com>
2021-05-11 20:52:59 +0300

    Add ProcessingDataUnitOfWork Tests

---
+++ src/DB.Tests/ProcessingDataUnitOfWork.Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Xunit;
+
+using FluentAssertions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using Abstractions.Models;
+using Abstractions.Serialization;
+
+namespace DB.Tests
+{
+    public class ProcessingDataUnitOfWorkTests : IDisposable
+    {
+
+        public ProcessingDataUnitOfWorkTests()
+        {
+            _conn = new SqliteConnection("Filename=:memory:");
+            _conn.Open();
+            var builder = new DbContextOptionsBuilder<OutboxContext>();
+            builder.UseSqlite(_conn);
+            var options = builder.Options;
+            _ctx = new OutboxContext(options);
+        }
+
+        [Fact(DisplayName = "ProcessingDataUnitOfWork cant be created with null context.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateWithNullContext()
+        {
+
+            // Arrange
+            var ctx = (OutboxContext)null!;
+            var logger = new Mock<ILogger<ProcessingDataUnitOfWork>>();
+            var serializer = new Mock<ISerializer<IProcessingData>>();
+
+            // Act
+            var exception = Record.Exception(() => new ProcessingDataUnitOfWork(ctx, logger.Object, serializer.Object));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+            _conn.Close();
+            _conn.Dispose();
+        }
+
+        private readonly OutboxContext _ctx;
+        private SqliteConnection _conn;
+    }
+}
