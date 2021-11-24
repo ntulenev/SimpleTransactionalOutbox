c4ddcadd13689bbfdfa4b5af7323975515719757
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-24 08:18:58 +0300

    DB.Tests -> remove namespaces

--- src/DB.Tests/OutboxFetcher.Tests.cs
+++ src/DB.Tests/OutboxFetcher.Tests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Linq;
-using System.Threading;
-using System.Collections.Generic;
-
-using Xunit;
+﻿using Xunit;
 
 using FluentAssertions;
 
--- src/DB.Tests/ProcessingDataUnitOfWork.Tests.cs
+++ src/DB.Tests/ProcessingDataUnitOfWork.Tests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Linq;
-
-using Xunit;
+﻿using Xunit;
 
 using FluentAssertions;
 
