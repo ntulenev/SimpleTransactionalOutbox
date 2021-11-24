a195d709f0a6ea18a495816bc8f14c316cae5936
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-24 08:18:23 +0300

    DB -> remove namespaces

--- src/DB/OutboxFetcher.cs
+++ src/DB/OutboxFetcher.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Linq;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 using Abstractions.DB;
 using Abstractions.Models;
