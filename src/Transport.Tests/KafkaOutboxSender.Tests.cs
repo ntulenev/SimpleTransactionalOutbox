71c747f583703955344abedc3497f7bbf92f06dc
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-24 08:24:23 +0300

    Transport.Tests -> remove namespaces

--- src/Transport.Tests/KafkaOutboxSender.Tests.cs
+++ src/Transport.Tests/KafkaOutboxSender.Tests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Threading;
-
-using Confluent.Kafka;
+﻿using Confluent.Kafka;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
